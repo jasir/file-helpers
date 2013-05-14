acea38264adbfda1f158a8af4f774ab4bbda13b2
jasir <sirjarda2@gmail.com>
2013-05-14 17:44:13 +0200

    added Directory

---
+++ lib/Directory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace jasir\FileHelpers;
+
+class Directory {
+
+	/**
+	 * @param string $directory
+	 * @param bool $deleteDir
+	 * @return success
+	 * implementation from http://lixlpixel.org/recursive_function/php/recursive_directory_delete/
+	 */
+
+	static function deleteRecursive($directory, $deleteDir = TRUE) {
+		$directory = File::normalizeSlashes($directory);
+		if (substr($directory, -1) == '/') {
+			$directory = substr($directory, 0, -1);
+		}
+		if (!file_exists($directory) || !is_dir($directory)) {
+			return FALSE;
+		} elseif (is_readable($directory)) {
+			$handle = opendir($directory);
+			while (FALSE !== ($item = readdir($handle))) {
+				if ($item !== '.' && $item !== '..') {
+					$path = $directory . '/' . $item;
+					if (is_dir($path)) {
+						self::deleteRecursive($path);
+					} else {
+						unlink($path);
+					}
+				}
+			}
+
+			closedir($handle);
+			if ($deleteDir === TRUE) {
+				if (!rmdir($directory)) {
+					return FALSE;
+				}
+			}
+		}
+		return TRUE;
+	}
+}
---
+++ tests/DirectoryTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace jasir\FileHelpers;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../lib/Directory.php';
+
+class DirectoryTest extends \PHPUnit_Framework_TestCase {
+
+    function setup() {
+        $this->deleteDirs();
+        $this->createDirs();
+    }
+    
+    function tearDown() {
+        $this->deleteDirs();
+    }
+
+    function test_deleteRecursive() {
+        $dir = $this->getPath();
+        $this->assertFileExists("$dir/subdir/file.txt");
+        Directory::deleteRecursive($dir);
+        $this->assertFileNotExists("$dir/subdir/file.txt");
+        $this->assertFileNotExists("$dir");
+    }
+
+    function test_deleteRecursive_keepsRootdir() {
+        $dir = $this->getPath();
+        $this->assertFileExists("$dir/subdir/file.txt");
+        Directory::deleteRecursive($dir, FALSE);
+        $this->assertFileNotExists("$dir/subdir/file.txt");
+        $this->assertFileNotExists("$dir/subdir");
+        $this->assertFileExists("$dir");
+    }
+
+    
+    private function deleteDirs() {
+        $dir = $this->getPath();
+        @unlink("$dir/subdir/file.txt");
+        @rmdir("$dir/subdir");
+        @rmdir("$dir");
+        
+        
+    }
+    
+    private function createDirs() {
+        $dir = $this->getPath();
+        mkdir($dir);
+        mkdir("$dir/subdir");
+        file_put_contents("$dir/subdir/file.txt", "hello");
+    }
+    
+    private function getPath() {
+        return __DIR__ . '/delete';
+    }
+
+}
